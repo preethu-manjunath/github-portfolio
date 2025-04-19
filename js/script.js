2c85931b63cf8ac82ba9ba65fb40f81c3376cd81
Preethu Nath Manjunath <preethunathmanjunath@gmail.com>
2025-04-19 21:26:37 +0000

    Push updated index.html and design changes

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const sections = document.querySelectorAll(".section");
-  
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry, index) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("fade-in");
-          entry.target.style.animationDelay = `${index * 0.2}s`;
-          observer.unobserve(entry.target);
-        }
-      });
-    }, { threshold: 0.2 });
-  
-    sections.forEach((section) => {
-      observer.observe(section);
+  const sections = document.querySelectorAll(".section");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry, index) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("fade-in");
+        entry.target.style.animationDelay = `${index * 0.2}s`;
+        observer.unobserve(entry.target);
+      }
     });
+  }, { threshold: 0.2 });
+
+  sections.forEach((section) => {
+    observer.observe(section);
   });
+});
